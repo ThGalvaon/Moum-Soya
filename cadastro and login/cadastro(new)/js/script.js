098246ebc50df55f22996a0251000441e541f825
LucasRodriguesCartaxo <lucas.cartaxo@sptech.school>
2024-10-23 18:22:45 -0300

    alterei o css do cadastro e deixei os nomes das classes mais simples

--- cadastro and login/cadastro(new)/js/script.js
+++ cadastro and login/cadastro(new)/js/script.js
@@ -1,7 +1,7 @@
 const mode = document.getElementById('mode_icon');
 
 mode.addEventListener('click', () => {
-    const form = document.getElementById('login_form');
+    const form = document.getElementById('cadastro_form');
     if (mode.classList.contains('fa-moon')) {
         mode.classList.remove('fa-moon');
         mode.classList.add('fa-sun');
@@ -67,7 +67,7 @@ function validateForm() {
 }
 
 // Adicionando o evento de submit ao formulário
-document.getElementById('login_form').addEventListener('submit', (event) => {
+document.getElementById('cadastro_form').addEventListener('submit', (event) => {
     if (!validateForm()) {
         event.preventDefault(); // Impede o envio do formulário se a validação falhar
     }
