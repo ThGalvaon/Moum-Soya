a31afd580188f076aa4c142c56de054afa45a6fc
PedroHCruzz <pedro.geraldo@sptech.school>
2024-10-26 15:27:39 -0300

    mudei as cores dos graficos e mudei o botão

--- dashboard/script.js
+++ dashboard/script.js
@@ -1,4 +1,4 @@
-const humidityData = [45, 50, 55, 65, 60, 69, 67, 75, 79, 74, 71];
+const humidityData = [45, 50, 55, 65, 60, 69, 67, 75, 78, 74,68];
 const timeLabels = ['12:00', '12:05', '12:10', '12:15', '12:20', '12:25', '12:30', '12:35', '12:40', '12:45', '12:50'];
 
 const sensorStatusData = {
@@ -7,8 +7,8 @@ const sensorStatusData = {
         data: [14, 2, 4],
         backgroundColor: [
             'rgba(0, 128, 0, 0.8)',
-            'rgba(60, 179, 113, 0.6)', 
-            'rgba(144, 238, 144, 0.4)' 
+            'rgb(255, 165, 0)', 
+            'rgb(255, 0, 0)' 
         ],
     }]
 };
@@ -19,8 +19,8 @@ const monthlyHumidityData = {
     datasets: [{
         label: 'Umidade Média (%)',
         data: [60, 64, 68, 69, 70, 72, 70, 76, 78, 74, 80, 82],
-        backgroundColor: 'rgba(60, 179, 113, 0.6)',
-        borderColor: 'rgba(0, 128, 0, 1)',
+        backgroundColor: 'rgba(0, 128, 0, 0.8)' ,
+        borderColor: 'rgba(60, 179, 113, 0.6)',
         borderWidth: 1,
     }]
 };
@@ -108,33 +108,41 @@ const monthlyHumidityChart = new Chart(ctxBar, {
     }
 });
 
-// Função para alternar a linha limite de 70
 document.getElementById('toggleButton').addEventListener('click', function () {
     const limitDataset = {
         label: 'Limite Máximo (70%)',
         data: new Array(monthlyHumidityData.labels.length).fill(70),
-        backgroundColor: 'rgba(255, 0, 0, 0.4)'
+        backgroundColor: 'rgb(255, 0, 0)'
     };
 
-    // Verifica se o dataset já foi adicionado ao gráfico
+ 
     const existingDatasetIndex = monthlyHumidityChart.data.datasets.findIndex(
         dataset => dataset.label === 'Limite Máximo (70%)'
     );
 
     if (existingDatasetIndex === -1) {
-        // Adiciona o dataset de limite ao gráfico
         monthlyHumidityChart.data.datasets.push(limitDataset);
         this.textContent = 'Remover Linha Limite';
     } else {
-        // Remove o dataset de limite do gráfico
         monthlyHumidityChart.data.datasets.splice(existingDatasetIndex, 1);
         this.textContent = 'Mostrar Linha Limite';
     }
 
-    // Atualiza o gráfico para exibir/ocultar o novo dataset
     monthlyHumidityChart.update();
 });
 
+document.getElementById("toggleButton").addEventListener("click", function () {
+    const button = this;
+    if (button.classList.contains("mostrar")) {
+        button.classList.remove("mostrar");
+        button.classList.add("remover");
+        button.textContent = "Mostrar Linha Limite";
+    } else {
+        button.classList.remove("remover");
+        button.classList.add("mostrar");
+        button.textContent = "Remover Linha Limite";
+    }
+});
 
 document.getElementById('tela2').style.display = 'none'
 document.getElementById('home').style.display = 'none'
@@ -145,8 +153,6 @@ function ver(){
     document.getElementById('tela2').style.display = 'block'
     document.getElementById('dash-alert').style.display = 'none'
     document.getElementById('home').style.display = 'block'
-
-
 
     const alertData = {
         labels: ['13:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00'],
