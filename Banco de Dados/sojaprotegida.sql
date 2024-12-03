f608cf8ac44db941df266d979695fee98c4683e4
KawanFGsptech <kawan.gomes@sptech.school>
2024-12-03 14:02:57 -0300

    teste

--- Banco de Dados/sojaprotegida.sql
+++ Banco de Dados/sojaprotegida.sql
@@ -2,17 +2,22 @@ CREATE DATABASE sojaprotegida;
 USE sojaprotegida;
 
 -- CRIAÇÃO DAS TABELAS
-CREATE TABLE Usuario (
+CREATE TABLE usuario (
 	idUsuario int primary key auto_increment,
-	razaoSocial varchar(45),
-	nomeFantasia varchar(50),
+	razao_social varchar(45),
+	nome_fantasia varchar(50),
 	cnpj char(14),
-	representante varchar(45),
+	repre varchar(45),
 	email varchar(70),
 	senha varchar(30)
 );
 
-CREATE TABLE Endereco (
+select * from usuario;
+
+INSERT INTO usuario (razao_social, nome_fantasia, cnpj, repre, email, senha) VALUES
+('Empresa A Ltda', 'Fantasia A', '12345678000195', 'João Silva', 'contato@empresaA.com.br', 'Senha#2024');
+
+CREATE TABLE endereco (
 	idEndereco int primary key auto_increment,
 	cidade varchar(45) not null,
 	bairro varchar(45) not null,
@@ -21,16 +26,22 @@ CREATE TABLE Endereco (
 	cep char(8) not null,
 	qtdTalhoes int,
 	fkUsuario int,
-    foreign key (fkUsuario) references Usuario (idUsuario)
+    foreign key (fkUsuario) references usuario (idUsuario)
 );
+
+INSERT INTO endereco (cidade, bairro, logradouro, numero, cep, qtdTalhoes, fkUsuario) VALUES
+('União da Serra', 'Santa Maria', 'Avenida Paulista', '100', '01311000', 4, 1);
 
 CREATE TABLE Sensor (
 	idSensor int primary key auto_increment,
     local_talhao varchar(45),
     statusSensor varchar(45),
     fkEndereco int,
-    foreign key (fkEndereco) references Endereco (idEndereco)
+    foreign key (fkEndereco) references endereco (idEndereco)
 );
+
+INSERT INTO Sensor (local_talhao, statusSensor, fkEndereco) VALUES
+('Talhão 1', 'Ativo', 1);
 
 CREATE TABLE DadosSensor (
 	idDadosSensor int auto_increment,
@@ -41,11 +52,13 @@ CREATE TABLE DadosSensor (
 	dtCaptura datetime
 );
 
+select * from DadosSensor;
+
 CREATE TABLE Alerta (
 idAlerta int auto_increment,
 fkSensor int,
 fkDadosSensores int,
-primary key (idAlerta, fkSensor, fkDadosSensor),
+primary key (idAlerta, fkSensor, fkDadosSensores),
 foreign key (fkSensor) references Sensor (idSensor),
 foreign key (fkDadosSensores) references DadosSensor (idDadosSensor),
 statusAlerta varchar(45),
@@ -56,7 +69,7 @@ check (statusAlerta in('Umidade elevada', 'Umidade alta', 'Umidade crítica'))
 
 CREATE TABLE UsuarioAlerta (
 fkUsuario int,
-foreign key (fkusuario) references Usuario (idUsuario),
+foreign key (fkusuario) references usuario (idUsuario),
 fkAlerta int,
 foreign key (fkAlerta) references Alerta (idAlerta),
 fkSensor int,
@@ -66,34 +79,5 @@ foreign key (fkDadosSensores) references DadosSensor (idDadosSensor),
 primary key (fkUsuario, fkAlerta, fkSensor, fkDadosSensores)
 );
 
--- SELECIONAR TODOS OS DADOS DA TABELA USUARIO
-SELECT * FROM Usuario;
-
--- SELECIONAR TODOS OS DADOS DA TABELA USUARIO ORDENADOS POR NOME FANTASIA
-SELECT * FROM Usuario ORDER BY nomeFantasia;
-
--- SELECIONAR OS REPRESENTANTES QUE POSSUAM 'MARIA' NO NOME
-SELECT representante FROM Usuario
-WHERE representante LIKE '%Maria%';
-
--- SELECIONAR TODOS OS DADOS DA TABELA ENDEREÇO
-SELECT * FROM Endereco;
-
--- SELECIONAR OS DADOS DAS FAZENDAS QUE POSSUEM MAIS DE 10 HECTARES
-SELECT u.nomeFantasia, e.qtdTalhoes from Usuario as u join Endereco as e on e.fkUsuario = u.idUsuario where qtdTalhoes > 10;
-
--- SELECIONAR OS DADOS DOS SENSORES
-SELECT * FROM Sensor;
-
--- SELECIONAR OS DADOS DOS SENSORES QUE ESTÃO MONITORANDO UMIDADES ACIMA DE 40
-SELECT * FROM dadosSensor
-WHERE umidade > 40;
-
--- SELECIONAR O NOME DAS FAZENDAS E VERIFICAR OS SENSORES EM FUNCIONAMENTO
-select s.statusSensor, u.nomeFantasia from Sensor as s join Endereco as e on s.fkEndereco = e.idEndereco join Usuario as u on e.fkUsuario = u.idUsuario;
-
--- SELECIONAR OS DADOS DO SENSOR E CALCULAR AS TEMPERATURAS MÁXIMAS, MÍNIMAS E MÉDIAS  
-SELECT * FROM sensor;
-SELECT avg(umidade)FROM dadosSensor;
-SELECT min(umidade)FROM dadosSensor;
-SELECT max(umidade)FROM dadosSensor;
+INSERT INTO usuario (email, senha) VALUES
+('admin@admin', 'admin');
