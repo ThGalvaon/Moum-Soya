9eea6270948abf1c09ff98eb351e676051a2d9e6
KawanFGsptech <kawan.gomes@sptech.school>
2024-12-03 10:02:26 -0300

    Inserts atualizados

--- Banco de Dados/sojaprotegida.sql
+++ Banco de Dados/sojaprotegida.sql
@@ -67,62 +67,30 @@ primary key (fkUsuario, fkAlerta, fkSensor, fkDadosSensores)
 );
 
 -- SELECIONAR TODOS OS DADOS DA TABELA USUARIO
-SELECT * FROM usuario;
+SELECT * FROM Usuario;
 
--- SELECIONAR TODOS OS DADOS DA TABELA USUARIO ORDENADOS POR NOME
-SELECT * FROM usuario ORDER BY nome;
+-- SELECIONAR TODOS OS DADOS DA TABELA USUARIO ORDENADOS POR NOME FANTASIA
+SELECT * FROM Usuario ORDER BY nomeFantasia;
 
--- SELECIONAR OS USUÁRIOS QUE POSSUAM 'MARIA' NO NOME
-SELECT nome FROM usuario
-WHERE nome LIKE '%Maria%';
+-- SELECIONAR OS REPRESENTANTES QUE POSSUAM 'MARIA' NO NOME
+SELECT representante FROM Usuario
+WHERE representante LIKE '%Maria%';
 
--- SELECIONAR TODOS OS DADOS DA TABELA FAZENDA
-SELECT * FROM fazenda;
-
--- SELECIONAR O IDENTIFICADOR, NOME E CNPJ DA FAZENDA, NOME DO USUARIO E DATA DE CANCELAMENTO DE USUARIOS INATIVOS
-SELECT f.idfazenda, f.nome as 'Nome da Fazenda', u.nome as 'Nome do usuário', f.cnpj, f.dtCancelamento, status_usuario
-FROM usuario as u JOIN fazenda as f
-ON idusuario = fkUsuario
-WHERE status_usuario = 'Inativo';
+-- SELECIONAR TODOS OS DADOS DA TABELA ENDEREÇO
+SELECT * FROM Endereco;
 
 -- SELECIONAR OS DADOS DAS FAZENDAS QUE POSSUEM MAIS DE 10 HECTARES
-SELECT * FROM fazenda
-WHERE hectares > 10;
-
--- SELECIONAR OS DADOS DOS USUARIOS ATIVOS E DAS FAZENDAS QUE POSSUEM MAIS DE 5 SENSORES
-SELECT * FROM fazenda JOIN usuario
-WHERE qtd_sensores > 5 and status_usuario = 'Ativo';
+SELECT u.nomeFantasia, e.qtdTalhoes from Usuario as u join Endereco as e on e.fkUsuario = u.idUsuario where qtdTalhoes > 10;
 
 -- SELECIONAR OS DADOS DOS SENSORES
-SELECT * FROM sensor;
+SELECT * FROM Sensor;
 
 -- SELECIONAR OS DADOS DOS SENSORES QUE ESTÃO MONITORANDO UMIDADES ACIMA DE 40
 SELECT * FROM dadosSensor
 WHERE umidade > 40;
 
-INSERT INTO sensor (localSensor, statusSensor, fkFazenda) VALUES
-('Local 31', null, 16);
-
-UPDATE sensor SET statusSensor = null
-  WHERE idSensor = 17;
-
 -- SELECIONAR O NOME DAS FAZENDAS E VERIFICAR OS SENSORES EM FUNCIONAMENTO
-SELECT f.nome as 'Nome fazenda',
-ifnull(statusSensor, 'Verificar sensor') as 'Status do sensor'
-FROM fazenda as f JOIN sensor
-ON fkFazenda = idFazenda;
-
-
--- SELECIONAR O NOME DAS FAZENDAS, O IDENTIFICADOS DOS SENSORES E SE A UMIDADE ESTÁ DENTRO DA NORMALIDADE
-SELECT f.nome as 'Nome da fazenda', idSensor, CASE 
-  WHEN umidade > 50 THEN 'Verificar local do sensor, umidade elevada!'
-  ELSE 'Umidade dentro da normalidade'
-  END as Umidade
-  FROM fazenda as f JOIN sensor
-  ON fkFazenda = idFazenda
-  LEFT JOIN dadosSensor
-  ON fkSensor = idSensor
-  ORDER BY nome;
+select s.statusSensor, u.nomeFantasia from Sensor as s join Endereco as e on s.fkEndereco = e.idEndereco join Usuario as u on e.fkUsuario = u.idUsuario;
 
 -- SELECIONAR OS DADOS DO SENSOR E CALCULAR AS TEMPERATURAS MÁXIMAS, MÍNIMAS E MÉDIAS  
 SELECT * FROM sensor;
