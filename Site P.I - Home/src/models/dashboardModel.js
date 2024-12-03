cec09cbc327ba592fdb75adefd9fa4210de1954c
ThGalvaon <thiago.neves@sptech.school>
2024-12-02 23:15:57 -0300

    finalizando controllers models e routes da dashboard

--- Site P.I - Home/src/models/dashboardModel.js
+++ Site P.I - Home/src/models/dashboardModel.js
@@ -1,10 +1,28 @@
-var database = require("../database/config")
+var database = require("../database/config");
+
+function inserirDadosSensor(idSensor, umidade) {
+    var instrucaoSql = `
+        INSERT INTO dadosSensor (fkSensor, umidade)
+        VALUES (${idSensor}, ${umidade});
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+function inserirAlerta(idSensor, statusAlerta, umidade) {
+    var instrucaoSql = `
+        INSERT INTO alerta (fkSensor, statusAlerta, umidadeAlerta)
+        VALUES (${idSensor}, '${statusAlerta}', '${umidade}');
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
 
 function obterDadosUmidade(idSensor) {
     var instrucaoSql = `
         SELECT umidade, dtCaptura
         FROM dadosSensor
-        WHERE fkSensor = ?;
+        WHERE fkSensor = ${idSensor};
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
@@ -14,13 +32,15 @@ function obterAlertas(idSensor) {
     var instrucaoSql = `
         SELECT statusAlerta, umidadeAlerta, dtAlerta
         FROM alerta
-        WHERE fkSensor = ?;
-    `;    
+        WHERE fkSensor = ${idSensor};
+    `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 
 module.exports = {
+    inserirDadosSensor,
+    inserirAlerta,
     obterDadosUmidade,
-    obterAlertas,
+    obterAlertas
 };
